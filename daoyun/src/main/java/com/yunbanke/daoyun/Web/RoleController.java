03f1dcfde3c29b94b51dda5b44a7f8715f101246
DogeZhang <zhangyz86@Hotmail.com>
2020-04-10 19:54:02 +0800

    修改UserController、RoleController

---
+++ daoyun/src/main/java/com/yunbanke/daoyun/Web/RoleController.java
@@ -0,0 +1,27 @@
+package com.yunbanke.daoyun.Web;
+
+import com.yunbanke.daoyun.Domain.entity.Role;
+import com.yunbanke.daoyun.Persistence.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+
+@RequestMapping("/role")
+@RestController
+@CrossOrigin
+public class RoleController {
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @RequestMapping("/addRole")
+    public void addRole(){
+        Role role = new Role();
+        role.setRole_createtime(new Date());
+        role.setRole_description("just description");
+        role.setRole_name("just name.");
+        System.out.println(role.getRole_name());
+        roleRepository.save(role);
+    }
+}
